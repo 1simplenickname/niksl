ac1307f0092bce91778f90f6356a35d3b7628a11
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-11-30 13:29:02 +0200

    Initial commit

--- index.php
+++ index.php
@@ -1,90 +1,49 @@
 <?php
+/**
+ * @package    Joomla.Site
+ *
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-    include('php/template.php');
+/**
+ * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
+ */
+define('JOOMLA_MINIMUM_PHP', '5.3.10');
 
-?>
+if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
+{
+	die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
+}
 
-<!doctype html>
-<html lang="en">
+// Saves the start time and memory usage.
+$startTime = microtime(1);
+$startMem  = memory_get_usage();
 
-    <head>
+/**
+ * Constant that is checked in included files to prevent direct access.
+ * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
+ */
+define('_JEXEC', 1);
 
-        <title>Niks Ļ</title>
-        <!--UTF-8 character support-->
-        <meta charset="UTF-8">
-        <?php template::generateHeader() ?>
+if (file_exists(__DIR__ . '/defines.php'))
+{
+	include_once __DIR__ . '/defines.php';
+}
 
-    </head>
+if (!defined('_JDEFINES'))
+{
+	define('JPATH_BASE', __DIR__);
+	require_once JPATH_BASE . '/includes/defines.php';
+}
 
-    <body>
+require_once JPATH_BASE . '/includes/framework.php';
 
-        <div id="container">
+// Set profiler start time and memory usage and mark afterLoad in the profiler.
+JDEBUG ? JProfiler::getInstance('Application')->setStart($startTime, $startMem)->mark('afterLoad') : null;
 
-            <div id="content">
+// Instantiate the application.
+$app = JFactory::getApplication('site');
 
-                <div id="contactsScreen">
-
-                    <div id="photo"></div>
-                    <div id="name">Niks Ļeoņenko</div>
-
-                    <div id="contactsDetail">
-
-                        <span id="callMe" onclick="showPhone()">call me</span><br>
-                        <span id="emailMe" onclick="showEmail()">e-mail me</span><br>
-                        <span id="findMe" onclick="showAddress()">find me</span><br>
-                        <br>
-                        <div id="247">anywhere, anytime</div>
-
-                    </div>
-
-                </div>
-
-        </div>
-
-        <?php template::indexNav() ?>
-
-        </div>
-
-        <!--Scripts-->
-        <script>
-
-            let callMe = document.getElementById("callMe");
-            let emailMe = document.getElementById("emailMe");
-            let findMe = document.getElementById("findMe");
-
-
-            function showPhone() {
-
-                if (callMe.innerHTML === "call me") {
-                    callMe.innerHTML = "+37126858216";
-                } else {
-                    callMe.innerHTML = "call me";
-                }
-
-            }
-
-            function showEmail() {
-
-                if (emailMe.innerHTML === "e-mail me") {
-                    emailMe.innerHTML = "niks.leonenko@icloud.com";
-                } else {
-                    emailMe.innerHTML = "e-mail me";
-                }
-
-            }
-
-            function showAddress() {
-
-                if (findMe.innerHTML === "find me") {
-                    findMe.innerHTML = "kaivas iela 50/4 - 73a";
-                } else {
-                    findMe.innerHTML = "find me";
-                }
-
-            }
-
-        </script>
-
-    </body>
-
-</html>
+// Execute the application.
+$app->execute();
--- php/endSession.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include("classes.php");
-
-    niksl::endSession();
-
-    header("Location: ../reviews");
